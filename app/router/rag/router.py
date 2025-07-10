2364c46140bfd32d1beeda01f2d90d42e2fd8484
santanu <s.m.23102004@gmail.com>
2025-07-10 10:01:17 +0530

    change the get request of /query to post request

--- app/router/rag/router.py
+++ app/router/rag/router.py
@@ -1,18 +1,19 @@
-from fastapi import APIRouter, Query, HTTPException, status
+from fastapi import APIRouter, HTTPException, status
 from app.core.rag import RAGPipeline
 from app.models.output import RAGResponse
+from app.models.input import QueryRequest
 
 router: APIRouter = APIRouter()
 rag_pipeline = RAGPipeline()
 
 
-@router.get(path="/query")
-async def query(q: str | None = Query(None, description="Query RAG")):
-    if not q:
+@router.post(path="/query")
+async def query(request: QueryRequest):
+    if not request.query.strip():
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="query parameter can't be empty",
+            detail="`query` field can't be empty",
         )
-    output: RAGResponse = rag_pipeline.run(query=q, top_k=2)
+    output: RAGResponse = rag_pipeline.run(query=request.query, top_k=2)
 
     return output
